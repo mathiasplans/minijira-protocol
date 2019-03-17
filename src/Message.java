6e77e6c4877517e68a29323bad29208ca9afc701
Mathias Plans <mathiasplans15@gmail.com>
2019-03-17 15:55:04 +0200

    Fix a typo

--- src/Message.java
+++ src/Message.java
@@ -37,7 +37,7 @@ public abstract class Message {
                     sendMessage(new String(data), MessageType.ERROR);
                 break;
             case SETSTATUSTASK:
-                if(!updateTimeTask(data))
+                if(!setStatusTask(data))
                     sendMessage(new String(data), MessageType.ERROR);
                 break;
             default:
